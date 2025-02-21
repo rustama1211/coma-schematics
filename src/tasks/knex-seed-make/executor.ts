21e7866e5ee820e36e97f566e4fb56b331ee3bdb
selen <rustama@maxsol.id>
2025-02-22 03:42:45 +0800

    chore(update): add knex migration make and seed

--- src/tasks/knex-seed-make/executor.ts
+++ src/tasks/knex-seed-make/executor.ts
@@ -56,7 +56,7 @@ export default function (
     try {
       await execute([
         'knex',
-        'migrate:make',
+        'seed:make',
         '-x',
         'ts',
         '--seeds-directory',
